811fc4bbc605538f878343f3684fc6513572b496
Novikoff1997 <andreytoys@yandex.ru>
2024-11-19 01:15:26 +0300

    fix: navbar script

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="css/style.css" />
     <title>Aliance</title>
   </head>
-  <body>
+  <body class="front-page">
     <div class="mobile-menu">
       <ul class="mobile-menu-nav">
         <li class="mobile-menu-nav-item">
@@ -97,7 +97,7 @@
       </a>
       <ul class="header-nav">
         <li class="header-nav-item">
-          <a href="#" class="header-nav-link">О компании</a>
+          <a href="about.php" class="header-nav-link">О компании</a>
         </li>
         <li class="header-nav-item">
           <a href="#" class="header-nav-link">Контрактное производство</a>
@@ -534,199 +534,6 @@
       </div>
     </section>
     <!-- /.blog -->
-    <section class="section cta">
-      <div class="bg-grey section-cta">
-        <img src="./img/cta-img/cta-image.png" alt="call to action" class="cta-image" />
-        <div class="cta-form-wrapper container">
-          <form action="#" class="cta-form">
-            <h2 class="section-title cta-form-title">Хотите сотрудничать?</h2>
-            <p class="cta-form-text">
-              Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие
-              вопросы и поможем даже в самых сложных случаях!
-            </p>
-            <div class="input-group-wrapper">
-              <div class="input-group">
-                <input id="user-name" type="text" class="input" placeholder=" " />
-                <label class="input-group-label" for="user-name">Имя</label>
-              </div>
-              <div class="input-group">
-                <input id="user-phone" type="tel" class="input" placeholder=" " />
-                <label class="input-group-label" for="user-phone">Номер телефона</label>
-              </div>
-            </div>
-            <!-- /.input-group-wrapper -->
-            <div class="cta-form-footer">
-              <button type="submit" class="button cta-form-button">Оставить заявку</button>
-              <div class="notify">
-                <svg class="notify-icon" width="12px" height="16px">
-                  <use href="./img/sprite.svg#notify-icon"></use>
-                </svg>
-                <p class="notify-text">
-                  Обращаясь к нам вы получаете не только профессиональную работу, но и абсолютную
-                  конфиденциальность информации!
-                </p>
-              </div>
-            </div>
-            <!-- /.cta-form-footer -->
-          </form>
-        </div>
-        <!-- /.cta-form-wrapper -->
-      </div>
-    </section>
-    <!-- /.cta -->
-    <footer class="footer">
-      <div class="container">
-        <div class="footer-top">
-          <svg class="logo-svg footer-logo">
-            <use href="./img/sprite.svg#logo-dark"></use>
-          </svg>
-          <a href="tel:+74996861014" class="footer-phone">+7 (499) 686-10-14</a>
-          <div class="footer-info">
-            <svg class="address-icon" width="12px" height="16px">
-              <use href="./img/sprite.svg#address-icon"></use>
-            </svg>
-            <address class="footer-info-address">г. Мосвка, Холодильный пер. 4к1с8</address>
-          </div>
-          <!-- /.footer-info -->
-          <div class="footer-info">
-            <svg class="email-icon" width="16px" height="12px">
-              <use href="./img/sprite.svg#mail-icon"></use>
-            </svg>
-            <a href="mailto:a.dragunov@tdaliance.ru" class="footer-info-email">a.dragunov@tdaliance.ru</a>
-          </div>
-          <!-- /.footer-info -->
-          <div class="footer-social">
-            <a class="footer-social-link" href="">
-              <svg class="footer-social-icon" width="19px" height="12px">
-                <use href="./img/sprite.svg#vk-icon"></use>
-              </svg>
-            </a>
-            <a class="footer-social-link" href="">
-              <svg class="footer-social-icon" width="13px" height="13px">
-                <use href="./img/sprite.svg#inst-icon"></use>
-              </svg>
-            </a>
-          </div>
-        </div>
-        <!-- /.footer-top -->
-      </div>
-      <hr color="#ebebf0" class="footer-seporator" />
-      <div class="container">
-        <div class="footer-bottom">
-          <div class="footer-menu-wrapper">
-            <h2 class="footer-menu-title">Контрактное производство</h2>
-            <ul class="footer-menu-list footer-menu-column-2">
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Автомобильная химия</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Бытовая химия</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Дезинфицирующие средства</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Пищевые аэрозоли</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Косметическая продукция</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Краски аэрозольные</a>
-              </li>
-            </ul>
-          </div>
-          <!-- /.footer-menu-wrapper -->
-          <div class="footer-menu-wrapper">
-            <h2 class="footer-menu-title">Собственные марки</h2>
-            <ul class="footer-menu-list">
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Автохимия AG-Tech</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link">Автохимия AG-Tech</a>
-              </li>
-            </ul>
-          </div>
-          <!-- /.footer-menu-wrapper -->
-          <div class="footer-menu-wrapper">
-            <ul class="footer-menu-list footer-menu-list-pad-0">
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link footer-menu-link-bold">О компании</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link footer-menu-link-bold">Новости</a>
-              </li>
-              <li class="footer-menu-item">
-                <a href="#" class="footer-menu-link footer-menu-link-bold">Контакты</a>
-              </li>
-            </ul>
-          </div>
-          <!-- /.footer-menu-wrapper -->
-        </div>
-      </div>
-      <!-- /.container -->
-      <hr color="#ebebf0" class="footer-seporator" />
-      <div class="container">
-        <div class="footer-wrapper">
-          <div class="footer-legal">
-            <p class="footer-copyright">&copy; 2022 «Aliance Production». Все права защищены.</p>
-            <a href="#" class="footer-policy">Политики конфиденциальности</a>
-          </div>
-          <!-- /.footer-legal -->
-          <div class="footer-author">
-            <span class="made-in">Сделано в</span>
-            <svg width="52px" height="11px">
-              <use href="./img/sprite.svg#ruso"></use>
-            </svg>
-          </div>
-        </div>
-        <!-- /.footer-wrapper -->
-      </div>
-      <!-- /.container -->
-    </footer>
-    <div class="modal">
-      <div class="modal-dialog">
-        <div class="modal-dialog-header">
-          <h2 class="modal-title">Есть вопросы?</h2>
-          <a href="" class="modal-close" data-toggle="modal">
-            <svg class="close-icon" width="24" height="24">
-              <use href="./img/sprite.svg#close"></use>
-            </svg>
-          </a>
-        </div>
-        <p class="modal-text">
-          Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие вопросы
-          и поможем даже в самых сложных случаях!
-        </p>
-        <form action="" class="modal-form">
-          <div class="input-group-wrapper input-group-vartical">
-            <div class="input-group modal-input-group">
-              <input id="modal-user-name" type="text" class="input modal-input" placeholder=" " />
-              <label class="input-group-label modal-input-label" for="modal-user-name">Имя</label>
-            </div>
-            <div class="input-group modal-input-group">
-              <input id="modal-user-phone" type="tel" class="input modal-input" placeholder=" " />
-              <label class="input-group-label modal-input-label" for="modal-user-phone">Номер телефона</label>
-            </div>
-          </div>
-          <!-- /.input-group-wrapper -->
-          <div class="modal-form-footer">
-            <button type="submit" class="button modal-form-button">Оставить заявку</button>
-            <div class="notify">
-              <svg class="notify-icon" width="12px" height="16px">
-                <use href="./img/sprite.svg#notify-icon"></use>
-              </svg>
-              <p class="notify-text">
-                Обращаясь к нам вы получаете не только профессиональную работу, но и абсолютную
-                конфиденциальность информации!
-              </p>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-    <script src="js/swiper-bundle.min.js"></script>
-    <script src="js/main.js"></script>
+    <?php require_once('templates/footer.php') ?>
   </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ const logoLight = document.querySelector(".logo-light");
 const logoDark = document.querySelector(".logo-dark");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const isFront = document.body.classList.contains("front-page");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logoLight.style.display = "none";
-  logoDark.style.display = "block";
 };
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
-  logoLight.style.display = "block";
-  logoDark.style.display = "none";
+};
+
+const changeNavHeight = (height) => {
+  navbar.style.height = height;
 };
 
 //Функция открывания меню
@@ -31,7 +32,10 @@ const closeMenu = (event) => {
 };
 
 window.addEventListener("scroll", () => {
-  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  this.scrollY > 1 ? changeNavHeight("4.5rem") : changeNavHeight("5.875rem");
+  if (isFront) {
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  }
 });
 
 mMenuToggle.addEventListener("click", (event) => {
