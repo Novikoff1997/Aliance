d99e05267a6325c3730cad7fc927a0f8d2d8a05c
Novikoff1997 <andreytoys@yandex.ru>
2024-11-24 22:24:00 +0300

    add: page templates

--- templates/footer.php
+++ templates/footer.php
@@ -211,4 +211,6 @@
     <script src="js/swiper-bundle.min.js"></script>
     <script src="js/just-validate.production.min.js"></script>
     <script src="js/main.js"></script>
+  </body>
+</html>
     
---
+++ templates/trademarks-section.php
@@ -0,0 +1,29 @@
+<section class="section trademarks">
+      <div class="container">
+        <div class="seporator"></div>
+        <!-- <h2 class="section-title">Собственные торговые марки</h2> -->
+        <div class="trademarks-cards">
+          <div class="trademarks-card">
+            <svg class="icon trademarks-image ag-tech-image">
+              <use href="./img/sprite.svg#agtech"></use>
+            </svg>
+            <h3 class="trademarks-card-title">Автохимия AG-Tech</h3>
+            <p class="trademarks-card-text">
+              Для современного мира разбавленное изрядной долей эмпатии, рациональное мышление создаёт
+              предпосылки для поставленных обществом задач.
+            </p>
+          </div>
+          <div class="trademarks-card">
+            <svg class="icon trademarks-image">
+              <use href="./img/sprite.svg#ap"></use>
+            </svg>
+            <h3 class="trademarks-card-title">Автохимия AP</h3>
+            <p class="trademarks-card-text">
+              Для современного мира разбавленное изрядной долей эмпатии, рациональное мышление создаёт
+              предпосылки для поставленных обществом задач.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.trdemarks -->
